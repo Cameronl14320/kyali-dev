d61e3e90787d7f605ff51b1054d58f0bd2dd2421
Cameron Li <cameronl14320@gmail.com>
2021-08-29 18:52:11 +1200

    Section

---
+++ Components/Section.tsx
@@ -0,0 +1,46 @@
+import React, {FunctionComponent} from "react";
+import {Box, Button, Grid, Link} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+
+const sectionStyle = makeStyles({
+    section: {
+        display: 'block',
+        background: 'black',
+        height: '80vh',
+        color: 'white',
+        padding: '10px',
+    },
+    section_button: {
+        color: 'white',
+        margin: '10px',
+        padding: '10px 40px 10px 40px',
+        border: '2px solid',
+        minWidth: '150px',
+        minHeight: '50px',
+    },
+    section_content: {
+        background: 'white',
+        minWidth: '200px',
+        minHeight: '100px',
+    },
+});
+
+export default function Section(props: {sectionId: string}) {
+    const classes = sectionStyle();
+    return (
+        <Box id={"section-" + props.sectionId} className={classes.section}>
+            <Grid
+                container
+                direction={"row"}
+                justifyContent={"center"}
+                alignItems={"center"}
+            >
+                <Box className={classes.section_content}>
+                </Box>
+                <Button className={classes.section_button}>
+                    {props.sectionId}
+                </Button>
+            </Grid>
+        </Box>
+    )
+}
